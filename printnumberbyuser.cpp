b9ec3dbea45fafe51cdb75a675a8649f9701a343
Shishir Tambe <93042473+Coldman22@users.noreply.github.com>
2022-07-25 17:42:19 +0530

    Add files via upload
    In this C++ problem the number is input user to print.

---
+++ printnumberbyuser.cpp
@@ -0,0 +1,13 @@
+// Print number entered by user.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+    cout<<"enter an inetger: ";
+    cin>> number;
+
+    cout<<"You have entered: "<<number;
+    return 0;
+}

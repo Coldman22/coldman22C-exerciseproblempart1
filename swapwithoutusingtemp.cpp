c91cfac98eb1baa02c427b7876ac86959b7645e2
Shishir Tambe <93042473+Coldman22@users.noreply.github.com>
2022-07-25 17:49:11 +0530

    Add files via upload
    In this C++ problem we will see how to swap numbers without using temporary variables.

---
+++ swapwithoutusingtemp.cpp
@@ -0,0 +1,18 @@
+//swap woithout using temporary variable.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a=5 , b=10;
+    cout<<"Before swapping."<<endl;
+    cout<<"a= "<<a<<", b = "<<b<<endl;
+
+    a=a+b;
+    b=a-b;
+    a=a-b;
+
+    cout<<"After swapping."<<endl;
+    cout<<"a= "<<a<<", b = "<<b<<endl;
+    return 0;
+}

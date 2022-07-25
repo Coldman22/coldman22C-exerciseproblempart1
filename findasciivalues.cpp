b11c35d595780b40f2b84837f64d551bcdbeff48
Shishir Tambe <93042473+Coldman22@users.noreply.github.com>
2022-07-25 17:50:24 +0530

    Add files via upload
    In this C++ problem we will see how to print the ASCII values of character.

---
+++ findasciivalues.cpp
@@ -0,0 +1,12 @@
+// Print ASCII value in C++
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char c;
+    cout<<"Enter the character: ";
+    cin>>c;
+    cout<<"ASCII values of "<<c<<" is "<<int(c);
+    return 0;
+}

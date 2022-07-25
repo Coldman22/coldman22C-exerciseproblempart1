c8cae264012b1aec000b17e4cebce973490c0391
Shishir Tambe <93042473+Coldman22@users.noreply.github.com>
2022-07-25 17:43:22 +0530

    Add files via upload
    In this C++ problem we see how to add two integers.

---
+++ addtwonubers.cpp
@@ -0,0 +1,14 @@
+// add two numbers.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double first_number , second_number, sum;
+    cout<<"Enter 2 integer: ";
+    cin>>first_number>>second_number;
+
+    sum= first_number+second_number;
+    cout<<first_number<<"+"<<second_number<<"="<<sum;
+    return 0;
+}
